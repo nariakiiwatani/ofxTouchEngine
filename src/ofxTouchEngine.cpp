39fbbd5074a1226abf8ea840ba28bcbc3842dac1
nariakiiwatani <nariakiiwatani@annolab.com>
2021-06-07 15:43:19 +0900

    successfully load a tox file

--- src/ofxTouchEngine.cpp
+++ src/ofxTouchEngine.cpp
@@ -1,5 +1,6 @@
 #include "ofxTouchEngine.h"
 #include "ofLog.h"
+#include "ofUtils.h"
 
 void ofxTouchEngine::eventCallbackGlobal(TEInstance * instance, TEEvent event, TEResult result, int64_t start_time_value, int32_t start_time_scale, int64_t end_time_value, int32_t end_time_scale, void * info)
 {
@@ -15,7 +16,7 @@ void ofxTouchEngine::linkCallbackGlobal(TEInstance * instance, TELinkEvent event
 
 ofxTouchEngine::ofxTouchEngine()
 {
-	TEResult result = TEInstanceCreate(eventCallbackGlobal, linkCallbackGlobal, this, &instance_);
+	auto result = TEInstanceCreate(eventCallbackGlobal, linkCallbackGlobal, this, &instance_);
 	if(result != TEResultSuccess) {
 		ofLogError("ofxTouchEngine") << "failed to create instance";
 	}
@@ -23,12 +24,35 @@ ofxTouchEngine::ofxTouchEngine()
 
 ofxTouchEngine::~ofxTouchEngine()
 {
-	TERelease(&instance_);
+	if(instance_) {
+		TERelease(&instance_);
+	}
 }
 
 void ofxTouchEngine::eventCallback(TEEvent event, TEResult result, int64_t start_time_value, int32_t start_time_scale, int64_t end_time_value, int32_t end_time_scale)
 {
+	switch(event) {
+		case TEEventInstanceDidLoad:
+			is_loaded_ = true;
+			break;
+	}
 }
 void ofxTouchEngine::linkCallback(TELinkEvent event, const char *identifier)
 {
+}
+
+bool ofxTouchEngine::load(const std::filesystem::path &filepath)
+{
+	is_loaded_ = false;
+	auto result = TEInstanceLoad(instance_, ofToDataPath(filepath, true).c_str(), TETimeExternal);
+	if(result != TEResultSuccess) {
+		ofLogError("ofxTouchEngine") << "failed to load a tox file";
+		return false;
+	}
+	result = TEInstanceResume(instance_);
+	if(result != TEResultSuccess) {
+		ofLogError("ofxTouchEngine") << "failed to resume instance";
+		return false;
+	}
+	return true;
 }
--- src/ofxTouchEngine.h
+++ src/ofxTouchEngine.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "TEInstance.h"
+#include "ofConstants.h"
 
 class ofxTouchEngine
 {
@@ -8,8 +9,12 @@ public:
 	ofxTouchEngine();
 	~ofxTouchEngine();
 
+	bool load(const std::filesystem::path &filepath);
+	bool isLoaded() const { return is_loaded_; }
+
 private:
-	TEInstance *instance_;
+	TEInstance *instance_ = nullptr;
+	bool is_loaded_ = false;
 
 	void eventCallback(TEEvent event, TEResult result, int64_t start_time_value, int32_t start_time_scale, int64_t end_time_value, int32_t end_time_scale);
 	void linkCallback(TELinkEvent event, const char *identifier);
