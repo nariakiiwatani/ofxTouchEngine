c87227374471f721173f4c73f4c14d307711636b
nariakiiwatani <nariakiiwatani@annolab.com>
2021-06-08 15:57:34 +0900

    move template implementations to cpp

--- src/ofxTEObject.cpp
+++ src/ofxTEObject.cpp
@@ -1,4 +1,8 @@
 #include "ofxTEObject.h"
+#include "ofxTouchEngine.h"
+#include "ofGLUtils.h"
+#include "ofMath.h"
+#include "TEObject.h"
 
 void ofxTEObject::setup(ofxTouchEngine &engine, const std::string &identifier)
 {
@@ -7,6 +11,27 @@ void ofxTEObject::setup(ofxTouchEngine &engine, const std::string &identifier)
 	context_ = engine.getContext();
 	identifier_ = identifier;
 }
+
+
+template<>
+void ofxTEObjectInput::setValue(const std::string &src)
+{
+	engine_->waitForFrame();
+	TouchObject<TELinkInfo> link;
+	auto result = TEInstanceLinkGetInfo(instance_, identifier_.c_str(), link.take());
+	if(result != TEResultSuccess) {
+		return;
+	}
+	assert(link.get()->scope == TEScopeInput
+		   && link.get()->type == TELinkTypeString
+		   && link.get()->domain == TELinkDomainParameter);
+	result = TEInstanceLinkSetStringValue(instance_, identifier_.c_str(), src.c_str());
+	if(result != TEResultSuccess) {
+		return;
+	}
+}
+
+
 void ofxTEObjectOutput::update()
 {
 	bool new_frame_arrived = false;
@@ -21,4 +46,47 @@ void ofxTEObjectOutput::notifyNewDataArrival()
 {
 	std::lock_guard<decltype(mtx_)> lock(mtx_);
 	new_frame_arrived_ = true;
+}
+
+
+template<>
+bool ofxTEObjectOutput::decodeTo(ofTexture &dst) const
+{
+	engine_->waitForFrame();
+	TouchObject<TELinkInfo> link;
+	auto result = TEInstanceLinkGetInfo(instance_, identifier_.c_str(), link.take());
+	if(result != TEResultSuccess) {
+		return false;
+	}
+	assert(link.get()->scope == TEScopeOutput
+		   && link.get()->type == TELinkTypeTexture
+		   && link.get()->domain == TELinkDomainOperator);
+	TouchObject<TEDXGITexture> dxgi_tex;
+	result = TEInstanceLinkGetTextureValue(instance_, identifier_.c_str(), TELinkValueCurrent, dxgi_tex.take());
+	if(result != TEResultSuccess || dxgi_tex.get() == nullptr) {
+		return false;
+	}
+	TEOpenGLTexture *te_tex = nullptr;
+	if(TEOpenGLContextCreateTexture(context_, dxgi_tex.get(), &te_tex) != TEResultSuccess) {
+		return false;
+	}
+	object_.take(te_tex);
+	dst.setUseExternalTextureID(TEOpenGLTextureGetName(te_tex));
+	auto &data = dst.getTextureData();
+	data.textureTarget = TEOpenGLTextureGetTarget(te_tex);
+	data.glInternalFormat = TEOpenGLTextureGetInternalFormat(te_tex);
+	data.width = TEOpenGLTextureGetWidth(te_tex);
+	data.height = TEOpenGLTextureGetHeight(te_tex);
+	data.bFlipTexture = !TETextureIsVerticallyFlipped(te_tex);
+	if(ofGLSupportsNPOTTextures()){
+		data.tex_w = data.width;
+		data.tex_h = data.height;
+	}
+	else {
+		data.tex_w = ofNextPow2(data.width);
+		data.tex_h = ofNextPow2(data.height);
+	}
+	data.tex_t = data.width / data.tex_w;
+	data.tex_u = data.height / data.tex_h;
+	return true;
 }
--- src/ofxTEObject.h
+++ src/ofxTEObject.h
@@ -1,15 +1,13 @@
 #pragma once
 
-#include "ofxTouchEngine.h"
 #include "ofTexture.h"
 #include <string>
 #include <mutex>
 #include "TouchObject.h"
 #include "TEInstance.h"
 #include "TEGraphicsContext.h"
-#include "ofGLUtils.h"
-#include "ofMath.h"
-#include "TEObject.h"
+
+class ofxTouchEngine;
 
 class ofxTEObject
 {
@@ -32,23 +30,7 @@ public:
 	void setValue(const Src &src);
 };
 
-template<>
-inline void ofxTEObjectInput::setValue(const std::string &src)
-{
-	engine_->waitForFrame();
-	TouchObject<TELinkInfo> link;
-	auto result = TEInstanceLinkGetInfo(instance_, identifier_.c_str(), link.take());
-	if(result != TEResultSuccess) {
-		return;
-	}
-	assert(link.get()->scope == TEScopeInput
-		   && link.get()->type == TELinkTypeString
-		   && link.get()->domain == TELinkDomainParameter);
-	result = TEInstanceLinkSetStringValue(instance_, identifier_.c_str(), src.c_str());
-	if(result != TEResultSuccess) {
-		return;
-	}
-}
+extern template void ofxTEObjectInput::setValue(const std::string&);
 
 class ofxTEObjectOutput : public ofxTEObject
 {
@@ -68,44 +50,5 @@ private:
 	void notifyNewDataArrival();
 };
 
-template<>
-inline bool ofxTEObjectOutput::decodeTo(ofTexture &dst) const
-{
-	engine_->waitForFrame();
-	TouchObject<TELinkInfo> link;
-	auto result = TEInstanceLinkGetInfo(instance_, identifier_.c_str(), link.take());
-	if(result != TEResultSuccess) {
-		return false;
-	}
-	assert(link.get()->scope == TEScopeOutput
-		   && link.get()->type == TELinkTypeTexture
-		   && link.get()->domain == TELinkDomainOperator);
-	TouchObject<TEDXGITexture> dxgi_tex;
-	result = TEInstanceLinkGetTextureValue(instance_, identifier_.c_str(), TELinkValueCurrent, dxgi_tex.take());
-	if(result != TEResultSuccess || dxgi_tex.get() == nullptr) {
-		return false;
-	}
-	TEOpenGLTexture *te_tex = nullptr;
-	if(TEOpenGLContextCreateTexture(context_, dxgi_tex.get(), &te_tex) != TEResultSuccess) {
-		return false;
-	}
-	object_.take(te_tex);
-	dst.setUseExternalTextureID(TEOpenGLTextureGetName(te_tex));
-	auto &data = dst.getTextureData();
-	data.textureTarget = TEOpenGLTextureGetTarget(te_tex);
-	data.glInternalFormat = TEOpenGLTextureGetInternalFormat(te_tex);
-	data.width = TEOpenGLTextureGetWidth(te_tex);
-	data.height = TEOpenGLTextureGetHeight(te_tex);
-	data.bFlipTexture = !TETextureIsVerticallyFlipped(te_tex);
-	if(ofGLSupportsNPOTTextures()){
-		data.tex_w = data.width;
-		data.tex_h = data.height;
-	}
-	else {
-		data.tex_w = ofNextPow2(data.width);
-		data.tex_h = ofNextPow2(data.height);
-	}
-	data.tex_t = data.width / data.tex_w;
-	data.tex_u = data.height / data.tex_h;
-	return true;
-}
+extern template bool ofxTEObjectOutput::decodeTo(ofTexture&) const;
+
